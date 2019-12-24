eb4098f5664c45a766830e9763c11ad635114775
jcoello <jeronimocoello@gmail.com>
2019-12-24 15:31:52 -0300

    Remove unnecesary adapter

--- app/src/main/java/com/example/miaminstantapp/UserFiltersFragment.kt
+++ app/src/main/java/com/example/miaminstantapp/UserFiltersFragment.kt
@@ -3,31 +3,18 @@ package com.example.miaminstantapp
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.miaminstantapp.domain.dtos.Ingredient
-import com.example.miaminstantapp.domain.entities.VolumeUnitEntity
 import com.example.miaminstantapp.view.BaseFragment
-import com.example.miaminstantapp.view.adapters.IngredientsChipAdapter
 import com.example.miaminstantapp.viewmodel.IUserIngredientsViewModel
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.fragment_user_filters.*
 
-class UserFiltersFragment : BaseFragment<IUserIngredientsViewModel, IUserIngredientsViewModel.State>(), IngredientsChipAdapter.IngredientItemClickListener {
-
-    private lateinit var suggestedIngredientsAdapter: IngredientsChipAdapter
+class UserFiltersFragment : BaseFragment<IUserIngredientsViewModel, IUserIngredientsViewModel.State>(){
 
     override fun getLayoutId(): Int = R.layout.fragment_user_filters
 
     override fun initViews() {
         viewModel.loadMasterData()
-
-        suggestedIngredientsAdapter = IngredientsChipAdapter(this)
-
-        suggestedIngredientsList.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = suggestedIngredientsAdapter
-        }
-
         super.initViews()
     }
 
@@ -45,7 +32,6 @@ class UserFiltersFragment : BaseFragment<IUserIngredientsViewModel, IUserIngredi
     }
 
     private fun logIngredients(ingredients: List<Ingredient>) {
-        suggestedIngredientsAdapter.setData(ingredients)
 
         chipsGroupSuggestedIngredients.removeAllViews()
         ingredients.forEach{
@@ -59,7 +45,7 @@ class UserFiltersFragment : BaseFragment<IUserIngredientsViewModel, IUserIngredi
             }
 
             chip.setOnClickListener{
-                onClick(ingredient)
+                onAddIngredient(ingredient)
                 chipsGroupSuggestedIngredients.removeView(chip)
             }
 
@@ -75,7 +61,7 @@ class UserFiltersFragment : BaseFragment<IUserIngredientsViewModel, IUserIngredi
         Toast.makeText(context, error, Toast.LENGTH_LONG).show()
     }
 
-    override fun onClick(ingredient: Ingredient) {
+    private fun onAddIngredient(ingredient: Ingredient) {
         viewModel.addIngredient(ingredient)
     }
 }
