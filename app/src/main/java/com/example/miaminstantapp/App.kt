7b6fd910e4765828d43b0914ee85b1cce5d42b81
jcoello <jeronimocoello@gmail.com>
2019-12-31 13:00:36 -0300

    Add google places autocomplete

--- app/src/main/java/com/example/miaminstantapp/App.kt
+++ app/src/main/java/com/example/miaminstantapp/App.kt
@@ -3,9 +3,17 @@ package com.example.miaminstantapp
 import com.example.miaminstantapp.injection.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import com.google.android.libraries.places.api.Places
 
 open class App: DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().create(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Places.initialize(applicationContext, "AIzaSyCEURjYf0e5I7qJUr7gnTbSVfjSzGvluTw")
+        Places.createClient(this)
     }
 }
