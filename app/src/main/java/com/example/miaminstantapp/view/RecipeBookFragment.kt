2d585fe4bbe1e11f13918163120e6bfbeb44ea04
jcoello <jeronimocoello@gmail.com>
2020-02-17 10:25:11 -0300

    Add recipe book adapter

--- app/src/main/java/com/example/miaminstantapp/view/RecipeBookFragment.kt
+++ app/src/main/java/com/example/miaminstantapp/view/RecipeBookFragment.kt
@@ -1,11 +1,16 @@
 package com.example.miaminstantapp.view
 
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.miaminstantapp.R
 import com.example.miaminstantapp.domain.entities.RecipeBookRecipeEntity
+import com.example.miaminstantapp.view.adapters.RecipeBookRecipeAdapter
 import com.example.miaminstantapp.viewmodel.IRecipeBookViewModel
+import kotlinx.android.synthetic.main.fragment_recipe_book.*
 
 class RecipeBookFragment: BaseFragment<IRecipeBookViewModel, IRecipeBookViewModel.State>() {
+
+    private lateinit var recipesAdapter: RecipeBookRecipeAdapter
 
     override fun getLayoutId(): Int = R.layout.fragment_recipe_book
 
@@ -16,12 +21,21 @@ class RecipeBookFragment: BaseFragment<IRecipeBookViewModel, IRecipeBookViewMode
     }
 
     private fun showRecipes(recipes: List<RecipeBookRecipeEntity>) {
-        Log.i("RECIPE_ADDED", recipes.first().name)
+        recipesAdapter.setRecipes(recipes)
     }
 
     override fun initViews() {
         super.initViews()
 
+        initRecipeList()
         viewModel.fetchRecipes()
+    }
+
+    fun initRecipeList() {
+        recipesAdapter = RecipeBookRecipeAdapter()
+        recipesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = recipesAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/example/miaminstantapp/view/adapters/RecipeBookRecipeAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.miaminstantapp.view.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.miaminstantapp.R
+import com.example.miaminstantapp.domain.entities.RecipeBookRecipeEntity
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_recipe_book_recipe.*
+
+class RecipeBookRecipeAdapter: RecyclerView.Adapter<RecipeBookRecipeAdapter.ViewHolder>() {
+
+    private val recipes = mutableListOf<RecipeBookRecipeEntity>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe_book_recipe, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(recipes[position])
+    }
+
+    override fun getItemCount(): Int = recipes.size
+
+    fun setRecipes(recipesList: List<RecipeBookRecipeEntity>) {
+        recipes.addAll(recipesList)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(override val containerView: View): RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind(recipe: RecipeBookRecipeEntity) {
+            recipeName.text = recipe.name
+        }
+    }
+}
