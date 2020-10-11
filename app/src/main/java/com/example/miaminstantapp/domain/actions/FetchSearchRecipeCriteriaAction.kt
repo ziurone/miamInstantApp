08e19951ec79bdd904d1c71a58373263f0f2209f
jcoello <jeronimocoello@gmail.com>
2020-10-11 13:59:54 -0300

    Add excluded ingredients to recipe search criteria.

--- app/src/main/java/com/example/miaminstantapp/domain/actions/FetchSearchRecipeCriteriaAction.kt
+++ app/src/main/java/com/example/miaminstantapp/domain/actions/FetchSearchRecipeCriteriaAction.kt
@@ -2,20 +2,24 @@ package com.example.miaminstantapp.domain.actions
 
 import com.example.miaminstantapp.domain.dtos.RecipeSearchCriteria
 import com.example.miaminstantapp.domain.entities.BranchEntity
+import com.example.miaminstantapp.domain.entities.ExcludedIngredientEntity
 import com.example.miaminstantapp.domain.entities.UserIngredientEntity
 import com.example.miaminstantapp.domain.repositories.IBranchRepository
+import com.example.miaminstantapp.domain.repositories.IExcludedIngredientRepository
 import com.example.miaminstantapp.domain.repositories.IUserMoneyRepository
 import com.example.miaminstantapp.domain.repositories.IngredientRepository
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.Function3
+import io.reactivex.functions.Function4
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class FetchSearchRecipeCriteriaAction @Inject constructor(
     private val ingredientRepository: IngredientRepository,
     private val moneyRepository: IUserMoneyRepository,
-    private val branchesRepository: IBranchRepository
+    private val branchesRepository: IBranchRepository,
+    private val excludedIngredientsRepository: IExcludedIngredientRepository
 ): BaseAction<IFetchSearchRecipeCriteriaAction.Result>(), IFetchSearchRecipeCriteriaAction {
 
     override fun fetch() {
@@ -25,14 +29,16 @@ class FetchSearchRecipeCriteriaAction @Inject constructor(
 
         val branchesSingle = branchesRepository.fetchAllFromLocal()
 
+        val excludedIngredientSingle = excludedIngredientsRepository.fetchAll()
 
-        Single.zip(userIngredientsSingle, userMoneySingle, branchesSingle,
-            Function3<List<UserIngredientEntity>, Int, List<BranchEntity>, IFetchSearchRecipeCriteriaAction.Result.Success> {
-                userIngredients: List<UserIngredientEntity>, userMoney: Int, branches: List<BranchEntity> ->
+        Single.zip(userIngredientsSingle, userMoneySingle, branchesSingle, excludedIngredientSingle,
+            Function4<List<UserIngredientEntity>, Int, List<BranchEntity>, List<ExcludedIngredientEntity>,IFetchSearchRecipeCriteriaAction.Result.Success> {
+                userIngredients: List<UserIngredientEntity>, userMoney: Int, branches: List<BranchEntity>, excludedIngredients: List<ExcludedIngredientEntity> ->
                     IFetchSearchRecipeCriteriaAction.Result.Success(RecipeSearchCriteria(
                         userIngredients,
                         userMoney,
-                        branches
+                        branches,
+                        excludedIngredients
                     ))
             }).subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
--- app/src/main/java/com/example/miaminstantapp/domain/dtos/RecipeSearchCriteria.kt
+++ app/src/main/java/com/example/miaminstantapp/domain/dtos/RecipeSearchCriteria.kt
@@ -1,10 +1,12 @@
 package com.example.miaminstantapp.domain.dtos
 
 import com.example.miaminstantapp.domain.entities.BranchEntity
+import com.example.miaminstantapp.domain.entities.ExcludedIngredientEntity
 import com.example.miaminstantapp.domain.entities.UserIngredientEntity
 
 data class RecipeSearchCriteria(
     val ingredients: List<UserIngredientEntity>,
     val userMoney: Int,
-    val branchesIds: List<BranchEntity>
+    val branchesIds: List<BranchEntity>,
+    val excludedIngredients: List<ExcludedIngredientEntity>
 )
