8516935b96a21fb10a94a886a44854d407ad2bff
jeronimo <jeronimocoello@gmail.com>
2021-05-29 18:08:52 -0300

    Change icons in presentation flow.

--- app/src/main/java/com/example/miaminstantapp/view/adapters/PresentationFragmentAdapter.kt
+++ app/src/main/java/com/example/miaminstantapp/view/adapters/PresentationFragmentAdapter.kt
@@ -17,9 +17,9 @@ class PresentationFragmentAdapter (val fa: FragmentActivity): FragmentStateAdapt
     override fun createFragment(position: Int): Fragment {
         val bundle = when(position) {
             0 -> bundleOf(DESCRIPTION_KEY to  fa.getString(R.string.presentation_first_step_text), ICON_KEY to R.drawable.ic_cheff, ENABLE_BUTTON_KEY to false )
-            1 -> bundleOf(DESCRIPTION_KEY to fa.getString(R.string.presentation_second_step_text),ICON_KEY to R.drawable.ic_fridge_medium, ENABLE_BUTTON_KEY to false )
-            2 -> bundleOf(DESCRIPTION_KEY to fa.getString(R.string.presentation_third_step_text), ICON_KEY to R.drawable.ic_cheff,ENABLE_BUTTON_KEY to false  )
-            3 -> bundleOf(DESCRIPTION_KEY to fa.getString(R.string.presentation_fourth_step_text), ICON_KEY to R.drawable.ic_delivery_big, ENABLE_BUTTON_KEY to true )
+            1 -> bundleOf(DESCRIPTION_KEY to fa.getString(R.string.presentation_second_step_text),ICON_KEY to R.drawable.ic_fridge_cheff, ENABLE_BUTTON_KEY to false )
+            2 -> bundleOf(DESCRIPTION_KEY to fa.getString(R.string.presentation_third_step_text), ICON_KEY to R.drawable.ic_recipe_book_ingredients, ENABLE_BUTTON_KEY to false  )
+            3 -> bundleOf(DESCRIPTION_KEY to fa.getString(R.string.presentation_fourth_step_text), ICON_KEY to R.drawable.ic_shopping_cart_cheff, ENABLE_BUTTON_KEY to true )
             else -> bundleOf(DESCRIPTION_KEY to "etc etc" )
         }
 
