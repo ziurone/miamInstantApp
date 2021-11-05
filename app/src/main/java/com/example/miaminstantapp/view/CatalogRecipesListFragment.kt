29caddcbcd593fc26ba0322da8b0a1938c429390
jeronimo <jeronimocoello@gmail.com>
2021-11-04 22:37:44 -0300

    Add suggested ingredients and chips styles

--- app/src/main/java/com/example/miaminstantapp/view/CatalogRecipesListFragment.kt
+++ app/src/main/java/com/example/miaminstantapp/view/CatalogRecipesListFragment.kt
@@ -1,11 +1,14 @@
 package com.example.miaminstantapp.view
 
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.miaminstantapp.R
+import com.example.miaminstantapp.domain.dtos.Ingredient
 import com.example.miaminstantapp.domain.relations.CatalogRecipeRelations
 import com.example.miaminstantapp.extensions.setFullScreen
 import com.example.miaminstantapp.routing.Launcher
@@ -13,8 +16,12 @@ import com.example.miaminstantapp.view.items.RecipeItem
 import com.example.miaminstantapp.view.recipedetail.CatalogRecipeDetailActivity.Companion.CATALOG_RECIPE_ID_KEY
 import com.example.miaminstantapp.view.recipedetail.CatalogRecipeDetailFragment
 import com.example.miaminstantapp.viewmodel.ICatalogRecipesListViewModel
+import com.google.android.material.chip.Chip
 import com.xwray.groupie.GroupAdapter
 import kotlinx.android.synthetic.main.fragment_catalog_recipes_list.*
+import kotlinx.android.synthetic.main.fragment_catalog_recipes_list.chipsGroupSuggestedIngredients
+import kotlinx.android.synthetic.main.fragment_catalog_recipes_list.ingredientsAutocompleteInput
+import kotlinx.android.synthetic.main.fragment_dispensary.*
 import kotlinx.android.synthetic.main.toolbar.*
 
 class CatalogRecipesListFragment: BaseFragment<ICatalogRecipesListViewModel, ICatalogRecipesListViewModel.State>() {
@@ -40,6 +47,7 @@ class CatalogRecipesListFragment: BaseFragment<ICatalogRecipesListViewModel, ICa
 
         initRecipeList()
         viewModel.fetchRecipes()
+        viewModel.fetchSuggestedIngredients()
     }
 
     private fun initRecipeList() {
@@ -54,6 +62,37 @@ class CatalogRecipesListFragment: BaseFragment<ICatalogRecipesListViewModel, ICa
         when(state) {
             is ICatalogRecipesListViewModel.State.FetchedRecipesSuccess -> showRecipes(state.catalogRecipeRelations)
             is ICatalogRecipesListViewModel.State.Error -> Unit
+            is ICatalogRecipesListViewModel.State.FetchSuggestedIngredientsSuccess -> showSuggestedIngredients(state.suggestedIngredients)
+        }
+    }
+
+    private fun showSuggestedIngredients(suggestedIngredients: List<Ingredient>) {
+        chipsGroupSuggestedIngredients.removeAllViews()
+        suggestedIngredients.forEach {
+            val chip = Chip(context)
+            chip.apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                text = it.name
+                isCloseIconVisible = false
+                textSize = 16f
+                setChipIconTintResource(R.color.secondary_light_100)
+                setChipBackgroundColorResource(R.color.secondary_light_100)
+                setOnCloseIconClickListener {
+                    it.visibility = View.GONE
+                    viewModel.fetchSuggestedIngredients()
+                }
+            }
+
+            chip.setOnClickListener {
+//                onAddIngredient(ingredient)
+                viewModel.fetchSuggestedIngredients()
+                chipsGroupSuggestedIngredients.removeView(chip)
+            }
+
+            chipsGroupSuggestedIngredients.addView(chip)
         }
     }
 
--- app/src/main/java/com/example/miaminstantapp/viewmodel/ICatalogRecipesListViewModel.kt
+++ app/src/main/java/com/example/miaminstantapp/viewmodel/ICatalogRecipesListViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.miaminstantapp.viewmodel
 
+import com.example.miaminstantapp.domain.dtos.Ingredient
 import com.example.miaminstantapp.domain.relations.CatalogRecipeRelations
 
 abstract class ICatalogRecipesListViewModel: BaseViewModel<ICatalogRecipesListViewModel.State>() {
     sealed class State {
         data class FetchedRecipesSuccess(val catalogRecipeRelations: List<CatalogRecipeRelations>): State()
         data class Error(val errorMessage: String): State()
+        data class FetchSuggestedIngredientsSuccess(val suggestedIngredients: List<Ingredient>) : State()
     }
 
     abstract fun fetchRecipes()
+    abstract fun fetchSuggestedIngredients()
+    abstract fun addIngredient(ingredient: Ingredient)
 }
